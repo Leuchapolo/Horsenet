d171cad15839f8e3fd31223ec67621e1523550f0
leucha <leucha@gmail.com>
2016-05-06 19:52:43 -0400

    Messing around with bootstrap

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -2,4 +2,12 @@ $(document).ready(function(){
 	$('#newsFeed').append('<div class = "homeObject">Here is some text to fill the page because it looks better if I have something here. Dont you agree? </div>')
 	$('#newsFeed').append('<div class = "homeObject">Here is some text to fill the page because it looks better if I have something here. Dont you agree? </div>')
 	$('#newsFeed').append('<div class = "homeObject">Here is some text to fill the page because it looks better if I have something here. Dont you agree? </div>')
+
+	$('#messages').append('<div class = "homeObject">Here is some text to fill the page because it looks better if I have something here. Dont you agree? </div>')
+	$('#messages').append('<div class = "homeObject">Here is some text to fill the page because it looks better if I have something here. Dont you agree? </div>')
+	$('#messages').append('<div class = "homeObject">Here is some text to fill the page because it looks better if I have something here. Dont you agree? </div>')
+
+	$('#favorites').append('<div class = "homeObject">Here is some text to fill the page because it looks better if I have something here. Dont you agree? </div>')
+	$('#favorites').append('<div class = "homeObject">Here is some text to fill the page because it looks better if I have something here. Dont you agree? </div>')
+	$('#favorites').append('<div class = "homeObject">Here is some text to fill the page because it looks better if I have something here. Dont you agree? </div>')favorites
 })
