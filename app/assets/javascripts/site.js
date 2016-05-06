8a793c25cbfcebfc47dd1ed7e86027de35e5440f
leucha <leucha@gmail.com>
2016-05-06 19:43:47 -0400

    Messing around with bootstrap

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,3 +1,5 @@
 $(document).ready(function(){
 	$('#newsFeed').append('<div class = "homeObject">Tacos</div>')
+	$('#newsFeed').append('<div class = "homeObject">Tacos</div>')
+	$('#newsFeed').append('<div class = "homeObject">Tacos</div>')
 })
