3046fbf0367130a26706ec2984e507ce3cee295b
leucha <leucha@gmail.com>
2016-05-06 19:50:20 -0400

    Messing around with bootstrap

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$('#newsFeed').append('<div class = "homeObject">Tacos</div>')
-	$('#newsFeed').append('<div class = "homeObject">Tacos</div>')
-	$('#newsFeed').append('<div class = "homeObject">Tacos</div>')
+	$('#newsFeed').append('<div class = "homeObject">Here is some text to fill the page because it looks better if I have something here. Dont you agree? </div>')
+	$('#newsFeed').append('<div class = "homeObject">Here is some text to fill the page because it looks better if I have something here. Dont you agree? </div>')
+	$('#newsFeed').append('<div class = "homeObject">Here is some text to fill the page because it looks better if I have something here. Dont you agree? </div>')
 })
