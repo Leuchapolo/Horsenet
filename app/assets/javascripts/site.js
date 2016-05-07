667c275e7194a16cc41fa60a076d6646c69252d7
leucha <leucha@gmail.com>
2016-05-06 20:05:31 -0400

    Messing around with bootstrap

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
+	$('#newsFeed').append('<div class = "homeObject"> <img src = "https://external-atl3-1.xx.fbcdn.net/safe_image.php?d=AQBzZmERGJRpQE5M&w=470&h=246&url=http%3A%2F%2Fi.huffpost.com%2Fgen%2F4154750%2Fimages%2Fo-HILLARY-CLINTON-facebook.jpg&cfs=1&upscale=1&sx=45&sy=0&sw=1911&sh=1000" height = "200" width = "200" alt = "Clinton"> Here is some text to fill the page because it looks better if I have something here. Dont you agree? </div>')
 	$('#newsFeed').append('<div class = "homeObject">Here is some text to fill the page because it looks better if I have something here. Dont you agree? </div>')
-	$('#newsFeed').append('<div class = "homeObject">Here is some text to fill the page because it looks better if I have something here. Dont you agree? </div>')
-	$('#newsFeed').append('<div class = "homeObject">Here is some text to fill the page because it looks better if I have something here. Dont you agree? </div>')
+	$('#newsFeed').append('<div class = "homeObject">Here is some text to fill the page because it looks better if I have something here. Dont you agree? I definitely agree with myself on that one because Im a very old spirit. I have lots of friends in other countries because of the internet. I like to write to penpals.</div>')
 
 	$('#messages').append('<div class = "homeObject">Here is some text to fill the page because it looks better if I have something here. Dont you agree? </div>')
 	$('#messages').append('<div class = "homeObject">Here is some text to fill the page because it looks better if I have something here. Dont you agree? </div>')
