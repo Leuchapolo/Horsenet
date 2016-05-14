faa29caf04519a80bf798b5b26a39d6706e9be31
leucha <leucha@gmail.com>
2016-05-14 15:57:09 -0400

    Finally think it should work

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -26,18 +26,18 @@ function setup(){
 }
 
 function setupMakeNewPost(){
-	$('#newsFeed').append(`<div class = "homeObject" id = "newsUnitNewPost"></div>`);
-	$('#newsUnitNewPost').append(`<div class = "newPostPhotoOptions"></div>`)
-	$('.newPostPhotoOptions').append(`<span id = "newPostPhotoOptionUpdateStatus"><a href = "">Update Status </a> </span>`)
-	$('.newPostPhotoOptions').append(`<span id = "newPostPhotoOptionAddPhotoVideo"><a href = "">Add Photo/Video</a> </span>`)
-	$('.newPostPhotoOptions').append(`<span id = "newPostPhotoOptionCreateAlbum"><a href = "">Create Album</a> </span>`)
-	$('#newsUnitNewPost').append(`<div class = "newPostTextProfPic"></div>`)
-	$('.newPostTextProfPic').append(`<img class = "newsPostHeaderProfPic" src = "https://s-media-cache-ak0.pinimg.com/564x/c6/7c/6f/c67c6fd92582294f775b16395de246ea.jpg" height = "40" width = "40">`)
-	$('.newPostTextProfPic').append(`<div class = "newPostTextBox"><textarea class = "newPostText" placeholder = "Write your post here"></textarea></div>`)
+	$('#newsFeed').append('<div class = "homeObject" id = "newsUnitNewPost"></div>');
+	$('#newsUnitNewPost').append('<div class = "newPostPhotoOptions"></div>')
+	$('.newPostPhotoOptions').append('<span id = "newPostPhotoOptionUpdateStatus"><a href = "">Update Status </a> </span>')
+	$('.newPostPhotoOptions').append('<span id = "newPostPhotoOptionAddPhotoVideo"><a href = "">Add Photo/Video</a> </span>')
+	$('.newPostPhotoOptions').append('<span id = "newPostPhotoOptionCreateAlbum"><a href = "">Create Album</a> </span>')
+	$('#newsUnitNewPost').append('<div class = "newPostTextProfPic"></div>')
+	$('.newPostTextProfPic').append('<img class = "newsPostHeaderProfPic" src = "https://s-media-cache-ak0.pinimg.com/564x/c6/7c/6f/c67c6fd92582294f775b16395de246ea.jpg" height = "40" width = "40">')
+	$('.newPostTextProfPic').append('<div class = "newPostTextBox"><textarea class = "newPostText" placeholder = "Write your post here"></textarea></div>')
 	autosize($('textarea'))
-	$('#newsUnitNewPost').append(`<div class = "newPostActions"></div>`)
-	$('.newPostActions').append(`<span class = "newPostActionsTagButtons"><span class="glyphicon glyphicon-tag newPostTagButton" aria-hidden="true"></span></span>`)
-	$(`.newPostActions`).append(`<button type="button" class="btn btn-primary postNewPostButton">Post</button>`);
+	$('#newsUnitNewPost').append('<div class = "newPostActions"></div>')
+	$('.newPostActions').append('<span class = "newPostActionsTagButtons"><span class="glyphicon glyphicon-tag newPostTagButton" aria-hidden="true"></span></span>')
+	$('.newPostActions').append('<button type="button" class="btn btn-primary postNewPostButton">Post</button>');
 }	
 
 
@@ -58,10 +58,18 @@ var fillNews = function(data){
 	//to sort you just do a sort function and go deep within the nested structure to find the created at for the post_tag
 	for (i in data){
 		//console.log(data[i])
-		$('#newsFeed').append(`<div class = "homeObject" id = "newsUnit${i}"></div>`)
-		$(`#newsUnit${i}`).prepend(`<div class = "newsPostHeaderInfo" id = "newsUnit${i}postHeaderInfo"></div>`)
-		$(`#newsUnit${i}postHeaderInfo`).append(`<div id = "newsUnit${i}postHeaderInfoPosters"> <span id = "newsUnit${i}postHeaderInfoText" class = "newsPostHeaderInfoText"><a href = "">${data[i].post.original_poster.name}</a></span></div>`)
-		var postersHtml = ``;
+		var newsUnitHTML = '<div class = "homeObject" id = "newsUnit' + i + '"></div>'
+		var newsUnit = "newsUnit" + i;
+		$('#newsFeed').append(newsUnitHTML)
+		var postHeaderInfo = "newsUnit" + i + "postHeaderInfo"
+		var postHeaderInfoHTML = '<div class = "newsPostHeaderInfo" id = "' + postHeaderInfo + '"></div>'
+		$("#" + newsUnit).prepend(postHeaderInfoHTML)
+		
+		var postHeaderInfoPosters = postHeaderInfo + "Posters"
+		var postHeaderInfoText = postHeaderInfo + "Text";
+		var postHeaderInfoPostersHTML = '<div id = "' + postHeaderInfoPosters + '"> <span id = "'  + postHeaderInfoText + '"  class = "newsPostHeaderInfoText">' + '<a href = "">' + data[i].post.original_poster.name + '</a></span></div>'
+		$("#" + postHeaderInfo).append(postHeaderInfoPostersHTML)
+		var postersHtml = '';
 		var taggedList = [];
 
 		var othersCount = 0;
@@ -72,24 +80,26 @@ var fillNews = function(data){
 			{
 				taggedList[j+1] = data[i].post_tags.tagged_objects[j].id 
 
-				if (postersHtml === ``){
-					postersHtml += ` with <a href = "">${data[i].post_tags.tagged_objects[j].name}</a>`;
+				if (postersHtml === ''){
+					postersHtml += ' with <a href = "">' + data[i].post_tags.tagged_objects[j].name + '</a>';
 				} else {
 					othersCount += 1;
 				}
 			}
 		}
 		if (othersCount > 0 ){
-			postersHtml += ` and <a href = ""> ${othersCount} others</a>` 
+			postersHtml += ' and <a href = "">' +  othersCount + 'others</a>'
 		}
-		postersHtml += `<br>`
-		$(`#newsUnit${i}postHeaderInfo`).prepend(`<img class = "newsPostHeaderProfPic" src = "https://s-media-cache-ak0.pinimg.com/564x/c6/7c/6f/c67c6fd92582294f775b16395de246ea.jpg" height = "40" width = "40">`)
-		$(`#newsUnit${i}postHeaderInfoText`).append(postersHtml);
-		$(`#newsUnit${i}postHeaderInfoPosters`).append(`<span class = "newsPostHeaderTime"> ${data[i].post.created_at} </span>`)
-
-		$(`#newsUnit${i}`).append(`<p class = "newsUnitText" id = "newsUnit${i}text"> ${data[i].post.text} </p>`)
-		$(`#newsUnit${i}`).append(`<div id = "newsUnit${i}likeBar" class = "likeBar"></div>`)
-		$(`#newsUnit${i}likeBar`).append(`<div id = "newsUnit${i}likeBarButtons" class = "likeBarButtons"></div>`)
-		$(`#newsUnit${i}likeBarButtons`).append(`<span class = "likeBarButton"><span class = "glyphicon glyphicon-thumbs-up" ></span></span><span class = "likeBarButton"><span class = "glyphicon glyphicon-comment" ></span></span><span class = "likeBarButton"><span class = "glyphicon glyphicon-share" ></span></span>`)
+		postersHtml += '<br>'
+		$("#" + postHeaderInfo).prepend('<img class = "newsPostHeaderProfPic" src = "https://s-media-cache-ak0.pinimg.com/564x/c6/7c/6f/c67c6fd92582294f775b16395de246ea.jpg" height = "40" width = "40">')
+		$("#" + postHeaderInfoText).append(postersHtml);
+		$("#" + postHeaderInfoPosters).append('<span class = "newsPostHeaderTime">' + data[i].post.created_at + ' </span>')
+		var newsUnitText = newsUnit + "text"
+		$("#" + newsUnit).append('<p class = "newsUnitText" id = "' + newsUnitText + '"> ' + data[i].post.text + ' </p>')
+		var newsUnitLikeBar = newsUnit + "likeBar"
+		$("#" + newsUnit).append('<div id = "' + newsUnitLikeBar +'" class = "likeBar"></div>')
+		var newsUnitLikeBarButtons = newsUnitLikeBar + "Buttons";
+		$("#" + newsUnitLikeBar).append('<div id = "' + newsUnitLikeBarButtons + '" class = "likeBarButtons"></div>')
+		$("#" + newsUnitLikeBarButtons).append('<span class = "likeBarButton"><span class = "glyphicon glyphicon-thumbs-up" ></span></span><span class = "likeBarButton"><span class = "glyphicon glyphicon-comment" ></span></span><span class = "likeBarButton"><span class = "glyphicon glyphicon-share" ></span></span>')
 	}
 }
