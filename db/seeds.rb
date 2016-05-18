2018e9115649c0bdcca2295d5d699b879bb0d833
leucha <leucha@gmail.com>
2016-05-18 00:25:29 -0400

    Made data into seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
-User.create!([
+u = User.new([
   {email: "taco@meatball.com", encrypted_password: "$2a$11$wHn3ndOHgzqoKiZBdUnXce/cABgnO2XxYUgEaHO30QW3Iz7G1H356", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2016-05-12 22:32:06", last_sign_in_at: "2016-05-12 22:32:06", current_sign_in_ip: "::1", last_sign_in_ip: "::1", name: "Taco", place_of_birth: nil, current_place_of_residence: nil, work: nil, current_show: nil, next_class: nil, specialization: nil, bio: nil, profile_picture: nil},
   {email: "mikesta@gmail.com", encrypted_password: "$2a$11$t.NPYMxF2pNYDbsg2dW52uFRT1.wmFtZS/wDznuRX2VHzLpqE9NBC", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 3, current_sign_in_at: "2016-05-12 23:25:29", last_sign_in_at: "2016-05-12 22:42:42", current_sign_in_ip: "::1", last_sign_in_ip: "::1", name: "Noah", place_of_birth: nil, current_place_of_residence: nil, work: nil, current_show: nil, next_class: nil, specialization: nil, bio: nil, profile_picture: nil},
   {email: "leucha@gmail.com", encrypted_password: "$2a$11$pQbx6bWN8fkxbmfV2Pv.8uBCKHZaTcZ18LRO6I0SOu6y7H.DHdtte", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 3, current_sign_in_at: "2016-05-14 22:13:26", last_sign_in_at: "2016-05-12 23:25:12", current_sign_in_ip: "::1", last_sign_in_ip: "::1", name: "Michael", place_of_birth: "Key Biscayne, FL", current_place_of_residence: "Wellington, FL", work: "Software Engineer at Google", current_show: "WEF", next_class: "High Juniors", specialization: "Jumpers", bio: "I love horses. And lamp.", profile_picture: "mikeyProfilePicture.jpg"}
 ])
+u.save!(validate: false)
 Horse.create!([
   {name: "Petunia", user_id: 1, birth_year: 2004, specialty: "Hunters", profile_picture: "horse-jumping.jpg"}
 ])
